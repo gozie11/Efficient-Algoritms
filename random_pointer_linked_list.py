831cc7a5907b8191ab6d4b190cb0a6544f97c1ef
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2023-10-10 01:06:03 -0500

    same as last

--- random_pointer_linked_list.py
+++ random_pointer_linked_list.py
@@ -1,1 +1,11 @@
 #Creatin a deep comy of a linked list with random pointers
+
+
+new_to_old = {}
+
+currernt = head
+while current:
+    new_to_old[current] = Node(current.val)
+    current = current.next
+
+current = head
