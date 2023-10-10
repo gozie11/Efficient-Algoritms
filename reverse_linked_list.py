315283864835cfa285902e61e3c297a69c0edb2e
gozie11 <gozieo7@gmail.com>
2023-10-10 18:26:07 -0500

    added code to implement a linked list to test with

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -2,9 +2,22 @@ class Node:
     def __init__(self, val, next=None, random=None):
         self.val = val
         self.next = next
-        self.random = random
+
+def print_list(head):
+    current = head
+    while current:
+        print(current.val)
+        current = current.next
 
 def main():
+
+    head = Node(7)
+    head.next = Node(13)
+    head.next.next = Node(11)
+    head.next.next.next = Node(10)
+    head.next.next.next.next = Node(1)
+    head.next.next.next.next.next = None
+
 
     prev, curr = None, head
 
@@ -13,5 +26,8 @@ def main():
         curr.next = prev
         prev = curr
         curr = next
-    
+    print_list(prev)
     return prev
+
+if __name__ == "__main__":
+    main()
