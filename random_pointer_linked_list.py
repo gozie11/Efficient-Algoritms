f151737748f852bf0ec2fa9642f6dcb9f1e70af0
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2023-10-10 01:11:09 -0500

    populated next, and random pointers in copy node

--- random_pointer_linked_list.py
+++ random_pointer_linked_list.py
@@ -1,11 +1,27 @@
 #Creatin a deep comy of a linked list with random pointers
 
 
-new_to_old = {}
 
-currernt = head
-while current:
-    new_to_old[current] = Node(current.val)
-    current = current.next
+class Node:
+    def __init__(self, val, next=None, random=None):
+        self.val = val
+        self.next = next
+        self.random = random
 
-current = head
+def main():     
+    head = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+    new_to_old = {None: None}
+
+    currernt = head
+    while current:
+        new_to_old[current] = Node(current.val)
+        current = current.next
+
+    current = head
+
+    while current:
+        new_to_old[current].next = new_to_old[current.next]
+        new_to_old[current].random = new_to_old[current.random]
+        current = current.next
+
+    return new_to_old[head]
