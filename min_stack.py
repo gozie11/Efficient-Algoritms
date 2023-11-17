fad0e5e60a7212d8121767047b641b1f428e5ae7
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2023-11-17 05:18:27 -0600

    Edit class metheds to use self identifier

--- min_stack.py
+++ min_stack.py
@@ -2,20 +2,20 @@ class MinStack:
 
   def __init__(self):
     """Initialize your data structures"""
-    stack = []
-    min_stack = []
+    self.stack = []
+    self.min_stack = []
 
   def push(self, val:int):
-    stack.append(val)
+    self.stack.append(val)
     min_val = min(val, min_stack[-1] if min_stack else val)
-    min_stack.append(min_stack)
+    self.min_stack.append(min_stack)
     
   def pop(self):
-    stack.pop()
-    min_stack.pop()
+    self.stack.pop()
+    self.min_stack.pop()
 
   def getMin(self) -> int:
-    return min_stack[-1]
+    return self.min_stack[-1]
     
   def top(self) -> int:
-    return stack[-1]
+    return self.stack[-1]
