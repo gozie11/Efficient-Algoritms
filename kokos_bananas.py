7837b2de875ba4ba5460098c5a7e70538019e005
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-02-03 16:57:27 -0600

    Create kokos_bananas.py

---
+++ kokos_bananas.py
@@ -0,0 +1,20 @@
+#What's the minimum number of bananas koko can eat per hour before the guards come back?
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+        while l < r:
+            k = (l+r)//2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p)/k)
+            if totalTime <= h: #if we did this in the right mount of time
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+        return res
+
+
