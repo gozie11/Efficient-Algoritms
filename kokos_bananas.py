1bab4dfd3cc4ce9dc88c2d0edfe39db6e862450a
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-02-03 20:47:47 -0600

    updated while loop to catch some edge cases

--- kokos_bananas.py
+++ kokos_bananas.py
@@ -4,7 +4,7 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
         res = r
-        while l < r:
+        while l <= r:
             k = (l+r)//2
 
             totalTime = 0
