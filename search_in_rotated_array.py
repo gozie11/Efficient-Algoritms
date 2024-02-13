fda4e03c26ca8597e356ddf9ea3388e5375439e4
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-02-13 16:24:23 -0600

    incorrect solution still...
    need to fix this to look at the right array.

--- search_in_rotated_array.py
+++ search_in_rotated_array.py
@@ -8,7 +8,7 @@ class Solution:
             mid = (l+r)//2
             if target == nums[mid]:
                 return mid
-            elif target >= nums[l]:
+            if target >= nums[l] or target < nums[mid] :
                 r = mid - 1
             else:
                 l = mid + 1
