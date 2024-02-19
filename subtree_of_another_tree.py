8f06355178d693afef371ff122b145181fb12d44
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-02-18 23:37:25 -0600

    Create subtree_of_another_tree.py
    I liked this problem. It was a decent way to start looking at comparing trees.

---
+++ subtree_of_another_tree.py
@@ -0,0 +1,24 @@
+def isSubTree(self, root:Optional[TreeNode], subTree: Option[TreeNode])-> bool:
+  if not subTree: return True #An emptry tree is a subtree of any tree
+  if not root: rutrn False #An emptry tree is not the main tree of any subroot. Get it? ask in the issues for a clearer explanation if necessary
+
+  if sameTree(root, subTree): return True 
+
+  # recursively call this function to chekc the children of the original tree against the subtree. Get it?
+  return(
+    isSubTree(root.left, subTree) 
+    or
+    isSubTree(root.right, subTree) 
+  )
+
+
+def sameTree(self, s: Optional[TreeNode], r: Optional[TreeNode]) -> bool:
+  #two empty trees are the same tree
+  if not s and not r: return True
+
+  # build up recursively if both trees exist
+  if s and r:
+    return(sameTree(s.left, r.left) and
+          sameTrre(s.right, r.right))
+
+  return False
