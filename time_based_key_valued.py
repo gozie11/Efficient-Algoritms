20d3b3b2faec1585151df0159544109d6fce5c81
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-02-23 17:28:55 -0600

    Create time_based_key_valued.py

---
+++ time_based_key_valued.py
@@ -0,0 +1,42 @@
+class TimeMap:
+
+    def __init__(self):
+        self.times = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.times:
+            self.times[key].append([value,timestamp])
+        else:
+            self.times[key]=[]
+
+    def get(self, key: str, timestamp: int) -> str:
+        #returns the greatest value <= timestamp
+        #binary serach the values at key
+
+        res, values = "", self.times.get(key, []) 
+        l,r = 0,len(values)-1
+        while l <= r:
+            m = (l+r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+
+# timebased ** All set timestamps are strictly increasing !! 
+# This means the values at key are ordered
+# {k:(v1,t1),(v2,t2)}
+# {foo:(bar,1),(bar2,4)}
+# get(foo@3) -> bar
+# get(foo@4) -> bar2
+# get(foo@5) -> bar2 # get reutrns the largest time stamp.prev value
+
