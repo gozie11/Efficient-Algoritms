814cbbe9e62e7f23c80df3285257a0a5b4f8cc87
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-03-03 20:41:29 -0600

    update comments in  invert_binary_tree.py

--- Trees/invert_binary_tree.py
+++ Trees/invert_binary_tree.py
@@ -2,18 +2,27 @@
 Humble brag... I solved this in 6 minutes from the first time I read it :)
 Glad to see progress. You can do it.
 
-"""
-Class Solution:
-  def invertTree(self, root:Optional[TreeNode]) -> Optional[TreeNode]:
-    if not root : return None
-  
-    temp_left = root.left
-    temp_right = root.right
-  
-    root.left = temp_right
-    root.right = temp_left
-  
-    self.invertTree(root.left)
-    self.invertTree(root.rigth)
+^not sure when that was... 03/03 I took a bit longer solving the problem. we're hooman.
 
-    return root
+"""
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # I want to swap the children of each node
+
+        #base case: we have no nodes
+        #break down: swap the nodes using temp values . call the function on the left and right children
+        #build up: set the left node = to a recursive call of the function and do the same for the right
+
+        if root == None:
+            return None
+        
+        temp_left = root.left 
+        temp_right = root.right 
+
+        root.left = temp_right
+        root.right = temp_left
+        
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+
+        return root
