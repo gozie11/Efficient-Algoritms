061970eb4eb7192c8f5d099d41e177126d936052
Gozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-03-12 07:36:23 -0500

    add variation of max_depth_of_tree.py
    Added comment to distill my thinking and highlight the parts I think are the most important to understanding the concept of building up a recursive solution.

--- Trees/max_depth_of_tree.py
+++ Trees/max_depth_of_tree.py
@@ -1,13 +1,25 @@
-# Import problem to break into the idea of recursion
-class TreeNode:
-  def __init__(self, val = 0, left = None, right = None):
-  self.val= val
-  self.left= left
-  self.right=right
-
+# Important problem to break into the idea of recursion
 class Solution:
   def maxDepth(self, root:Optional[treeNode]) -> int:
     if not root:
       return 0
     depth = 1
     return depth + max(self.maxDepth(root.left), self.maxDepth(root.right()))
+
+  # I came up with this solution in 13 minutes. Should've taken 5 since I've seen this.
+  # Initially I thought that I would need a helper function so started building it.
+  # That might show that I didn't fully understand the question.
+  # Understanding that ADDING the height of the current node (1) to the next recursive call is the most important
+  # thing for me to hold on to. It is how the recursion gets built up!
+  def maxDepth(self, root: Optional[TreeNode]) -> int:
+    # Base case : No node
+    # Break down : check the depth of both children
+    # build up : ADD the current node node height to 
+    # max (of the call on the subsequent recursive calls of left and right children)
+
+    if not root: return 0
+
+    left = 1 + self.maxDepth(root.left)
+    right = 1 + self.maxDepth(root.right)
+
+    return max(left,right)
