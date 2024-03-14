523f636c4bfac62eb9caaf9d890202abb14f26ea
Goosen1 <151769221+Goosen1@users.noreply.github.com>
2024-03-14 03:46:39 +0000

    resolve review comments

--- Trees/deserialize_serialize_binary_tree.py
+++ Trees/deserialize_serialize_binary_tree.py
@@ -10,22 +10,22 @@ class Solution():
       if not node:
         return "N"
       #preorder addition of nodes to our list
-      nodes.append(node.value)
-      nodes.append(dfs(node.left)) # I need to draw a picture that shows the nodes getting added to the list cause I'm confused
-      nodes.append(dfs(node.righ))
+      nodes.append(string(node.value))
+      dfs(node.left) # I need to draw a picture that shows the nodes getting added to the list cause I'm confused
+      dfs(node.righ)
       
-    dfs(node)
+    dfs(root)
     return nodes.join(",")
 
-  def deserialize(self, root):
-    nodes = root.split(",")
+  def deserialize(self, data):
+    nodes = data.split(",")
     self.i=0 # this is going to be used to track which node we are looking at in our serialized tree
 
     def dfs() :
       if nodes[self.i] == "N"
         self.i += 1
         return None 
-      new_tree = TreeNode(nodes[self.i])
+      new_tree = TreeNode(int(nodes[self.i]))
       self.i += 1
       new_tree.left = dfs()
       new_tree.right = dfs()
