51ef4eb0560071bb98b5f73e6ece5e8bf72a5418
Gozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-03-27 19:21:06 -0500

    Add explanation for use of list.copy() method

--- Backtracking/subsets.py
+++ Backtracking/subsets.py
@@ -5,6 +5,9 @@ class Solution:
         def dfs(i):
             #base case
             if i >= len(nums):
+                #It's important to use the copy method here because list are accessed by reference. 
+                #This means that all the list we append to result will be modified in subsequent recursive calls if we don't 
+                #ensure a coppy is appended to the answer instead of the original.
                 res.append(subset.copy())
                 return
             #left of decision tree
