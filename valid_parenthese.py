0b34ed99b908ac2cf891b1056abdbfd6d891897f
Gozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-06-09 13:06:28 -0500

    add attempt
    my skills degraded on this easy problem. No problem. Just keep working at it and return to her soon. Practice in a similar style as mango language learning.

--- valid_parenthese.py
+++ valid_parenthese.py
@@ -1,17 +1,43 @@
 
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 class Solution:
-
-
     def is_valid(self, s:str) -> bool:
         s = "([)]"
         complements = {')': '(', ']': '[', '}': '{'}
         stack = []
         for char in s:
-            if char in complements:
+            if char in complements: # complements here will only reference the keys of the dictionary!
                 if not stack or stack[-1] != complements[char]:
                     return False
                 stack.pop()
             else:
                 stack.append(char)
         return not stack
+
+
+complements = {')': '(', ']': '[', '}': '{'}
+
+
+#06/09/24 ... My skills worsened after 2 months off.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        if len(s) < 2: return False
+
+        dic = {")":"(",
+                "}":"{",
+                "]":"["
+                }
+
+        stack = []
+
+        for let in s:
+            if let in dic.values():
+                stack.append(let)
+            if let in dic.keys():
+                if not stack or stack[-1] != dic[let]: return False
+                else: stack.pop()
+        return False if stack else True
+
+
