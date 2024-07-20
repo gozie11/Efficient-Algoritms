e429465347163058f2e66105340d023b68d6d079
Gozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-07-20 14:09:50 -0500

    Add working solution
    Wrote this from memory so am happy. Every line is intuitive at this point. I need to schedule time to rewrite this solution and strengthen the ideas I've learned about BFS.

--- Graphs/closest_treasure_chest.py
+++ Graphs/closest_treasure_chest.py
@@ -1,3 +1,58 @@
+#2nd attempt. Working solution.
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        ROWS , COLS = len(grid), len(grid[0])
+
+        q = deque() # track where to visit next
+
+        visited = set()
+
+        def visit_next(r,c) -> None:
+            if (
+                r >= ROWS
+                or r < 0
+                or c >= COLS
+                or c < 0
+                or (r,c) in visited
+                or grid[r][c] == -1 # we can't traverse water buddy
+                
+            ):
+                return
+            q.append([r,c])
+            visited.add((r,c))
+            
+        # add all of the treasure to the q and mark them as visited
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    visited.add((r,c))
+                    q.append([r,c]) #I'm not sure why we add tuples to the set but lists to the deque. Maybe it's so that r,c = q.popleft() works properly
+
+        dist = 0 # we're starting from the treasure chests. Thus the starting distance to them is 0
+
+        while q:
+            for i in range(len(q)): # I'm assuming that this range does not change once initialized. This allows me to add elements to the deque without messing up the logic
+                r,c = q.popleft() # get the next location to visit
+                grid[r][c] = dist
+                visit_next(r+1,c)
+                visit_next(r-1,c)
+                visit_next(r, c-1)
+                visit_next(r, c+1)
+            dist += 1
+
+
+
+
+
+
+
+
+
+
+
+# 1st attempt
 class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
         # I tried to dfs a Bfs problem..
