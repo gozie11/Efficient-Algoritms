4e6795992a6da1edad95f87bb7780c7e3a509c79
Gozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-07-24 23:58:45 -0500

    Update kth_smallest_element.py

--- Trees/kth_smallest_element.py
+++ Trees/kth_smallest_element.py
@@ -1,7 +1,7 @@
-#Wooo got this one on my own. Relatively straight forward in order traversal of a bst.
+#WoooHoo! got this one on my own. Relatively straight forward in order traversal of a bst.
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+        #this means the root comes after the left child and before the right chile.
         in_order = []
 
         def dfs(root):
